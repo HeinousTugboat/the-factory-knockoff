abdc2f9302f925b098852e4e75e22d35b7b7ccff
HeinousTugboat <admin@heinousindustries.com>
2017-12-07 17:32:38 -0500

    feat: add basic details to item card

--- src/app/item-card/item-card.component.ts
+++ src/app/item-card/item-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ItemModel } from '../item.model';
 
 @Component({
   selector: 'app-item-card',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-card.component.scss']
 })
 export class ItemCardComponent implements OnInit {
+  @Input() item: ItemModel;
 
   constructor() { }
 
