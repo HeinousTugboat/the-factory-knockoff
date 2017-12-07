44e72c858e713cd91d912152c5de24aaee77cd21
HeinousTugboat <admin@heinousindustries.com>
2017-12-06 22:43:39 -0500

    feat: adds planned routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
+import { GameMenuComponent } from './panels/game-menu/game-menu.component';
+import { ManufacturingComponent } from './panels/manufacturing/manufacturing.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PurchasingComponent } from './panels/purchasing/purchasing.component';
+import { InventoryComponent } from './panels/inventory/inventory.component';
+import { SalesComponent } from './panels/sales/sales.component';
+import { AutomationComponent } from './panels/automation/automation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'purchasing', component: PurchasingComponent},
+  {path: 'manufacturing', component: ManufacturingComponent},
+  {path: 'inventory', component: InventoryComponent},
+  {path: 'sales', component: SalesComponent},
+  {path: 'automation', component: AutomationComponent},
+  {path: 'game', component: GameMenuComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
