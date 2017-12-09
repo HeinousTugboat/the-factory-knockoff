7b8161e0f308dc916b8469944c5aedff1fcaa5fa
HeinousTugboat <admin@heinousindustries.com>
2017-12-09 01:48:04 -0500

    feat: adds route observable and applies class to previous sibling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterEvent } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 import { DataService } from './data.service';
 import { ItemModel } from './item.model';
 import { RecipeModel } from './recipe.model';
 import { UpdateService } from './update.service';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'The Factory';
   logs: string[] = [];
   items: ItemModel[] = [];
   recipes: RecipeModel[] = [];
+  router$: Subscription;
+  logUpdates$: Subscription;
+  currentRoute: string;
 
-  constructor(public updateService: UpdateService, public dataService: DataService) {
-    updateService.log.subscribe(entry => {
-      // if (this.logs.length > 10) {
-        // this.logs.shift();
-      // }
+  constructor(private router: Router, public updateService: UpdateService, public dataService: DataService) {
+
+    this.router$ = router.events
+      .pipe(
+        filter(x => (x instanceof NavigationEnd)),
+        map((x: RouterEvent) => x.url.slice(1)),
+      ).subscribe(x => this.currentRoute = x);
+
+    this.logUpdates$ = updateService.log.subscribe(entry => {
       this.logs.push(entry);
     });
   }
@@ -28,5 +38,10 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.items = ItemModel.init();
     this.recipes = RecipeModel.init();
+  }
+
+  ngOnDestroy() {
+    this.router$.unsubscribe();
+    this.logUpdates$.unsubscribe();
   }
 }
