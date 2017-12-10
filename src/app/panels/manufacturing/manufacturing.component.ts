222ca10833ce5e48224be3119b9c6eb71cbfde60
HeinousTugboat <admin@heinousindustries.com>
2017-12-10 18:45:45 -0500

    feat(manu): first pass at manufacturing panel

--- src/app/panels/manufacturing/manufacturing.component.ts
+++ src/app/panels/manufacturing/manufacturing.component.ts
@@ -1,3 +1,4 @@
+import { Tiers } from '../../item.model';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 
@@ -11,12 +12,23 @@ import { RecipeModel } from '../../recipe.model';
 })
 export class ManufacturingComponent implements OnInit, OnDestroy {
   recipes: RecipeModel[];
+  tierLists: Map<Tiers, RecipeModel[]> = new Map;
+  tiers = Tiers;
   private recipes$: Subscription;
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
     this.recipes$ = this.dataService.recipes$.subscribe(recipes => {
-      this.recipes = recipes.filter(x => x.inputs.length && x.inputs.every(y => y && y.available));
+      this.recipes = recipes.filter(x => x.tier !== Tiers.RESOURCE)
+        .filter(x => x.inputs.length && x.inputs.every(y => y && y.available));
+      this.tierLists = new Map;
+      this.recipes.forEach(recipe => {
+        if (!this.tierLists.has(recipe.tier)) {
+          this.tierLists.set(recipe.tier, [recipe]);
+        } else {
+          this.tierLists.get(recipe.tier).push(recipe);
+        }
+      });
     });
   }
 
