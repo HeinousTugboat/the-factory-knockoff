49e05a01163c2fd4fb4f8f62d13f65d2d7a0e930
HeinousTugboat <admin@heinousindustries.com>
2017-12-15 13:20:04 -0500

    feat: adds initial structure and code for inventory panel

--- src/app/panels/inventory/inventory.component.ts
+++ src/app/panels/inventory/inventory.component.ts
@@ -12,7 +12,46 @@ import { ItemModel } from '../../models/item.model';
 export class InventoryComponent implements OnInit, OnDestroy {
   items: ItemModel[];
   private items$: Subscription;
+  toSell: Map<ItemModel, number> = new Map;
+  totals: { buy: number, sell: number, net: number, qty: number | null, buyQty: number, sellQty: number }
+    = { buy: 0, sell: 0, net: 0, qty: null, buyQty: 0, sellQty: 0 };
+
   constructor(private dataService: DataService) { }
+
+  toSellChange(item: ItemModel, amount: number) {
+    const qty = this.toSell.get(item) || 0;
+    // console.log('toPurchaseChange', item.current, qty, amount);
+    this.toSell.set(item, qty + amount + item.current >= 0 ? qty + amount : qty);
+    this.updateTotals();
+  }
+  
+  updateTotals() {
+    this.totals = Array.from(this.toPurchase.entries()).reduce((acc, item) => {
+      if (item[1] === 0) { return acc; }
+      if (!acc.qty) { acc.qty = 0; }
+      if (item[1] > 0) {
+        acc.buy -= item[1] * item[0].buyPrice;
+        acc.buyQty += item[1];
+      }
+      if (item[1] < 0) {
+        acc.sell -= item[1] * item[0].salePrice;
+        acc.sellQty += item[1];
+      }
+      acc.qty += item[1];
+      acc.net = acc.sell + acc.buy;
+      return acc;
+    }, { buy: 0, sell: 0, net: 0, qty: null, buyQty: 0, sellQty: 0 });
+  }
+
+  purchase() {
+    this.updateTotals();
+
+    for (const [item, qty] of Array.from(this.toSell.entries())) {
+      // item.current -= qty;
+    }
+    this.toSell = new Map;
+    this.updateTotals();
+  }
 
   ngOnInit() {
     this.items$ = this.dataService.items$.subscribe(items => {
