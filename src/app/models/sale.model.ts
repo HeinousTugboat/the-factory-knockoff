bdecae9722c415e0c4fe585147ecbafc03f72719
HeinousTugboat <admin@heinousindustries.com>
2017-12-16 16:28:27 -0500

    feat: adds prelim structure to sale.model

--- src/app/models/sale.model.ts
+++ src/app/models/sale.model.ts
@@ -1,5 +1,14 @@
 import { ItemModel } from './item.model';
 
 export class SaleModel {
+  label: string = Math.floor(Math.random() * 2 ** 32).toString(16).toUpperCase();
+  duration: number;
+  created = Date.now();
+  isOpen = true;
+  isSold = false;
+  isComplete = false;
 
+  constructor(items: ItemModel[]) {
+
+  }
 }
