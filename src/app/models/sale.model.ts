98bd9c494685e937e40d2c939d06c10c327bb6b0
HeinousTugboat <admin@heinousindustries.com>
2017-12-17 13:11:54 -0500

    feat: adds base methods to SaleModel

--- src/app/models/sale.model.ts
+++ src/app/models/sale.model.ts
@@ -1,14 +1,47 @@
-import { ItemModel } from './item.model';
+import { ItemModel, Tiers } from './item.model';
 
 export class SaleModel {
   label: string = Math.floor(Math.random() * 2 ** 32).toString(16).toUpperCase();
   duration: number;
+  due: number;
   created = Date.now();
   isOpen = true;
   isSold = false;
   isComplete = false;
+  value = 0;
 
-  constructor(public items: ItemModel[]) {
+  constructor(public items: Map<ItemModel, number>) {
+    for (const [item, qty] of items) {
+      if (item.current >= qty) {
+        item.current -= qty;
+        this.value += qty * item.salePrice;
+      } else {
+        throw new Error('Invalid Sale Quantity!');
+      }
+    }
+    this.duration = SaleModel.calculateDuration(items);
+    this.due = this.created + this.duration;
+  }
 
+  static calculateDuration(items: Map<ItemModel, number>): number {
+    let dur = 0;
+    for (const [item, qty] of items) {
+      const mod = SaleModel.getTierMod((item));
+      dur += mod * qty * item.salePrice + 3500;
+    }
+    return Math.floor(dur);
+  }
+
+  static getTierMod(item: ItemModel): number {
+    switch (item.tier) {
+      case Tiers.RESOURCE:
+      case Tiers.TIER_1:  return 120;
+      case Tiers.TIER_2:  return 240;
+      case Tiers.TIER_3:  return 360;
+      case Tiers.TIER_4:  return 480;
+      case Tiers.BUILDER: return 600;
+      case Tiers.TASKER:  return 660;
+      default:            return 0;
+    }
   }
 }
